ef8d7ab882e8bb827b4e7824d2fbdf13aeefc636
Caio Vasconcelos <caiovasconcelos01@live.com>
2025-12-30 20:12:28 -0300

    :sparkles: feat: ec2 init

---
+++ 01-ec2/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web_server" {
+ami           = "ami-0e2c8caa4b6378d8c"
+instance_type = "t2.micro"
+
+tags = {
+    Name        = var.instance_name
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+}
+}
---
+++ 01-ec2/variables.tf
@@ -0,0 +1,5 @@
+variable "instance_name" {
+description = "Instância EC2"
+type        = string
+default     = "Web-Server-1"
+}
